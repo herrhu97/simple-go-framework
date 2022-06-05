038e79bc5aa19dee5604299014c8834e01babcde
herrhu97 <herrhu@qq.com>
2022-06-05 18:01:13 +0800

    feat: 自定义groupcache实现一致性哈希

---
+++ cache/consistenthash/consistenthash_test.go
@@ -0,0 +1,53 @@
+package consistenthash
+
+import (
+	"sort"
+	"strconv"
+	"testing"
+)
+
+func TestHashing(t *testing.T) {
+	hash := New(3, func(key []byte) uint32 {
+		i, _ := strconv.Atoi(string(key))
+		return uint32(i)
+	})
+
+	// Given the above hash function, this will give replicas with "hashes":
+	// 2, 4, 6, 12, 14, 16, 22, 24, 26
+	hash.Add("6", "4", "2")
+
+	testCases := map[string]string{
+		"2":  "2",
+		"11": "2",
+		"23": "4",
+		"27": "2",
+	}
+
+	for k, v := range testCases {
+		tmp := hash.Get(k)
+		if tmp != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+	// Adds 8, 18, 28
+	hash.Add("8")
+
+	// 27 should now map to 8.
+	testCases["27"] = "8"
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+}
+
+func TestBinarySearch(t *testing.T) {
+	res := sort.Search(10, func(i int) bool {
+		return i > 10
+	})
+
+	t.Log(res)
+}
--- cache/go.mod
+++ cache/go.mod
@@ -2,4 +2,6 @@ module github.com/herrhu97/simple-go-framework/cache
 
 go 1.16
 
-require github.com/herrhu97/simple-go-framework v0.0.0-20220604083407-5077b831f52a
+replace github.com/herrhu97/simple-go-framework/log => ../log
+
+require github.com/herrhu97/simple-go-framework/log v0.0.0-00010101000000-000000000000
